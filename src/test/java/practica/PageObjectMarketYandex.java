c611afe39d3bdbff03cfc1b7e33ab6d8ec825a16
Starostin Alexandr <mailqagit@yandex.ru>
2020-07-31 19:12:17 +0300

    sleep 3000

--- src/test/java/practica/PageObjectMarketYandex.java
+++ src/test/java/practica/PageObjectMarketYandex.java
@@ -12,24 +12,21 @@ import java.util.List;
 
 public class PageObjectMarketYandex {
     //    private String selectorSearchItems="//h2[@class='organic__title-wrapper typo typo_text_l typo_line_m']";
-    private String selectorSearchItems = "//article";
-    private String selectorCssCheckBoxIOs = "[name='Платформа iOS'][type=checkbox]";
+//    private String selectorSearchItems = "//article";
+//    private String selectorCssCheckBoxIOs = "[name='Платформа iOS'][type=checkbox]";
     private String selectorXpathCheckBoxIOs = "//span[text()='iOS']";
     private String selectorXpathArticleText = "//article//h3/a";
-    private String selectorXpathDivResultPages = "//div[contains(@data-bem,\"itemsPerPage\")]";
+//    private String selectorXpathDivResultPages = "//div[contains(@data-bem,\"itemsPerPage\")]";
     private String selectorXpathButtonShowMorePages = "//button[text()='Показать ещё']";
-    private String selectorXpathForwardToNextPage = "//a[@aria-label='Следующая страница']";
-    private int i = 1;
+//    private String selectorXpathForwardToNextPage = "//a[@aria-label='Следующая страница']";
 //    private String selectorXpathForwardToNextPage = "//a[@class='button button_size_s button_theme_pseudo n-pager__button-next i-bem n-smart-link button_js_inited n-smart-link_js_inited']";
 //                                                    a[@class="button button_size_s button_theme_pseudo n-pager__button-next i-bem n-smart-link button_js_inited n-smart-link_js_inited"]
 //                                                            button button_size_m button_theme_pseudo i-bem button_js_inited
 
     private WebDriver driver;
     private List<WebElement> searchWebItems = new ArrayList<>();
-    private List<WebElement> resultWebItems = new ArrayList<>();
     private List<String> collectResults = new ArrayList<>();
     private WebElement filterCheckBoxIOs;
-    private WebElement forwardToNextPage;
     private WebElement buttonShowMorePages;
 
     public PageObjectMarketYandex(WebDriver driver, String search) {
@@ -37,29 +34,8 @@ public class PageObjectMarketYandex {
         this.driver.get("https://market.yandex.ru/catalog--mobilnye-telefony/54726/list?text=" + search);
     }
 
-    public List<WebElement> getSearchWebItems() {
-        return searchWebItems;
-    }
-
-    public List<String> getCollectResults() {
-      /*  String pageURL = driver.getCurrentUrl();
-        System.out.println(pageURL);
-//        driver.get(pageURL);
-        searchWebItems = driver.findElements(By.xpath(selectorXpathArticleText));
-        searchWebItems.stream().forEach(x -> collectResults.add(x.getAttribute("title")));*/
-        return collectResults;
-    }
-
-    public WebElement getForwardToNextPage() {
-        return forwardToNextPage;
-    }
-
     public WebElement getButtonShowMorePages() {
         return buttonShowMorePages;
-    }
-
-    public void clearCollectResult() {
-        collectResults.clear();
     }
 
     public void setFilterCheckBoxBoxIOs() {
@@ -73,26 +49,13 @@ public class PageObjectMarketYandex {
         try {
             buttonShowMorePages = driver.findElement(By.xpath(selectorXpathButtonShowMorePages));
             System.out.println("try");
-            /*try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }*/
-//            waitForLoad(driver);
-//            buttonShowMorePages.click();
+
         } catch (Exception e) {
             System.out.println("catch");
             buttonShowMorePages = null;
         }
         System.out.println("return " + (buttonShowMorePages != null));
         return buttonShowMorePages != null;
-    }
-
-    public void waitForLoad(WebDriver driver) {
-        ExpectedCondition<Boolean> pageLoadCondition = driver1 -> ((JavascriptExecutor) driver1).executeScript("return document.readyState").equals("complete");
-        WebDriverWait wait;
-        wait = new WebDriverWait(driver, 3);
-        wait.until(pageLoadCondition);
     }
 
     public boolean isTitleContainsIPhone() {
--- src/test/java/practica/Tests.java
+++ src/test/java/practica/Tests.java
@@ -11,32 +11,19 @@ public class Tests extends WebDriverSettings {
             " Установить фильтр (checkbox) iOS (айфон)." +
             " Убедится что на всех страницах (если их несколько), присутствуют только айфоны. ")
     public void testPOMarketYandexTelephonesCheckIPhone() {
-
         PageObjectMarketYandex pageObjectMarketYandex = new PageObjectMarketYandex(chromeDriver, "телефоны");
         pageObjectMarketYandex.setFilterCheckBoxBoxIOs();
 
-//        int i = 1;
-//        pageObjectMarketYandex.getTitleFromWebItems();
         while (pageObjectMarketYandex.isExistsButtonShowMorePages()) {
             try {
-                Thread.sleep(2000);
+                Thread.sleep(3000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             pageObjectMarketYandex.getButtonShowMorePages().click();
         }
-        /*do {
-            if (pageObjectMarketYandex.isExistsButtonShowMorePages()) {
 
-//                pageObjectMarketYandex.clickButton();
-//                pageObjectMarketYandex.getTitleFromWebItems();
-
-            } else break;
-        } while (true);*/
         Assertions.assertTrue(pageObjectMarketYandex.isTitleContainsIPhone());
-//        pageObjectMarketYandex.checkTitle();
     }
-
-
 }
 
